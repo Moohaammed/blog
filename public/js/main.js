8bc2fd5281b6fedf66901734727e224c41ecae31
Muhammad Tarek <tarek3529@gmail.com>
2020-05-04 18:18:44 +0200

    style modified

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,15 @@
-function justifyContent() {
-    var navBarHeight = document.querySelector('.navbar').getBoundingClientRect().height;
-    var location = window.location.href;
-    var page = location.split('/')[3]; // http://localhost:<port>/<page>/e/t/c
-    if (page == "") {
-        document.body.style.marginTop = 0+'px';
-    } else {
-        document.body.style.marginTop = navBarHeight+'px';
-    }
-}
+// function justifyContent() {
+var navbarHeight = document.querySelector('.navbar').getBoundingClientRect().height;
+    // var location = window.location.href;
+    // var page = location.split('/')[3]; // http://localhost:<port>/<page>/e/t/c
+    // if (page == "") {
+        // document.body.style.marginTop = 0+'px';
+    // } else {
+document.body.style.marginTop = navbarHeight+'px';
+    // }
+// }
 
-justifyContent()
+// justifyContent()
 
 function activateLink() {
     var location = window.location.href;
